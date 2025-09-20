773e9dce4b387e5a5dd1ceae0c8a1d2114240444
mynameisbob1928 <129603125+mynameisbob1928@users.noreply.github.com>
2025-09-20 21:15:39 +0100

    Complete overhaul to how the nbt detection works

--- src/main/java/me/mynameisbob1928/antinbt/InventoryEvents.java
+++ src/main/java/me/mynameisbob1928/antinbt/InventoryEvents.java
@@ -1,10 +1,12 @@
 package me.mynameisbob1928.antinbt;
 
 import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Container;
 import org.bukkit.enchantments.Enchantment;
@@ -17,8 +19,11 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
-import org.bukkit.inventory.meta.BundleMeta;
+import org.bukkit.inventory.meta.CrossbowMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import io.papermc.paper.datacomponent.DataComponentType;
+import io.papermc.paper.datacomponent.DataComponentTypes;
 
 public class InventoryEvents implements Listener {
 	@EventHandler
@@ -99,19 +104,88 @@ public class InventoryEvents implements Listener {
 		}
 	}
 
+	private final Set<DataComponentType> nbtToIgnore = Set.of(
+			DataComponentTypes.BANNER_PATTERNS, // Banner or shield patterns
+			DataComponentTypes.BASE_COLOR, // Shield colour
+			DataComponentTypes.CHARGED_PROJECTILES, // Crossbow projectiles
+			DataComponentTypes.CONTAINER, // Container contents (this is manually checked)
+			DataComponentTypes.CUSTOM_NAME, // Name set as if it were named by an anvil
+			DataComponentTypes.DAMAGE, // Durability removed from an item
+			DataComponentTypes.DYED_COLOR, // Dyed colour of a dyeable item (eg leather armour)
+			DataComponentTypes.ENCHANTMENTS, // Item enchantments
+			DataComponentTypes.INSTRUMENT, // Goat horn sound
+			DataComponentTypes.ITEM_NAME, // Other way of naming item (is not italic)
+			DataComponentTypes.LODESTONE_TRACKER,
+			DataComponentTypes.LORE,
+			DataComponentTypes.MAP_ID, // All (non-blank) maps have a map id
+			DataComponentTypes.NOTE_BLOCK_SOUND, // Sound that plays when a player head on a noteblock (can be customised)
+			DataComponentTypes.PROFILE, // Player head data
+			DataComponentTypes.REPAIR_COST,
+			DataComponentTypes.STORED_ENCHANTMENTS, // Enchantments applied to an item but do not have any effect, like enchantments on an enchanted book
+			DataComponentTypes.SUSPICIOUS_STEW_EFFECTS, // Effects applied when consumed
+			DataComponentTypes.TRIM, // Trims applied to armour
+			DataComponentTypes.WRITABLE_BOOK_CONTENT, // Book and quill content
+			DataComponentTypes.WRITTEN_BOOK_CONTENT, // Written book content
+
+			// Entity variants
+			DataComponentTypes.AXOLOTL_VARIANT,
+			DataComponentTypes.CAT_VARIANT,
+			DataComponentTypes.CHICKEN_VARIANT,
+			DataComponentTypes.COW_VARIANT,
+			DataComponentTypes.FOX_VARIANT,
+			DataComponentTypes.FROG_VARIANT,
+			DataComponentTypes.HORSE_VARIANT,
+			DataComponentTypes.LLAMA_VARIANT,
+			DataComponentTypes.PAINTING_VARIANT,
+			DataComponentTypes.PARROT_VARIANT,
+			DataComponentTypes.PIG_VARIANT,
+			DataComponentTypes.RABBIT_VARIANT,
+			DataComponentTypes.SALMON_SIZE,
+			DataComponentTypes.SHEEP_COLOR,
+			DataComponentTypes.SHULKER_COLOR,
+			DataComponentTypes.TROPICAL_FISH_BASE_COLOR,
+			DataComponentTypes.TROPICAL_FISH_PATTERN,
+			DataComponentTypes.TROPICAL_FISH_PATTERN_COLOR,
+			DataComponentTypes.VILLAGER_VARIANT,
+			DataComponentTypes.WOLF_COLLAR,
+			DataComponentTypes.WOLF_SOUND_VARIANT,
+			DataComponentTypes.WOLF_VARIANT
+
+	);
+
 	private boolean nbtPresent(ItemStack item) {
 		if (item == null)
 			return false;
+
+		if (Material.BUNDLE == item.getType()) { // completely block bundles
+			return true;
+		}
+
+		ItemStack defaultItem = ItemStack.of(item.getType());
+
 		if (!item.hasItemMeta()) {
-			return false;
+			return !item.matchesWithoutData(defaultItem, nbtToIgnore, true); // Also put it here since perhaps some data might not be checked by item meta
 		}
 
 		ItemMeta meta = item.getItemMeta();
 
+		if (item.isDataOverridden(DataComponentTypes.CHARGED_PROJECTILES) // Ensure that crossbows are only charged with either a firework or an arrow
+				&& meta instanceof CrossbowMeta crossbowMeta) {
+
+			for (ItemStack projectile : crossbowMeta.getChargedProjectiles()) {
+				if (projectile.getType() != Material.FIREWORK_ROCKET
+						&& projectile.getType() != Material.ARROW
+						&& projectile.getType() != Material.TIPPED_ARROW
+						&& projectile.getType() != Material.SPECTRAL_ARROW) {
+					return true;
+				}
+			}
+		}
+
 		if (meta instanceof BlockStateMeta) {
 			BlockState state = ((BlockStateMeta) meta).getBlockState();
 
-			if (state instanceof Container) {
+			if (state instanceof Container) { // Checks if containers (chests, barrels, dispensers, ect) have any items in them
 				Inventory inv = ((Container) state).getInventory();
 				for (ItemStack content : inv.getContents()) {
 					if (content != null && !content.isEmpty()) {
@@ -121,34 +195,13 @@ public class InventoryEvents implements Listener {
 			}
 		}
 
-		if (meta instanceof BundleMeta) {
-			BundleMeta bundleMeta = (BundleMeta) meta;
-			if (!bundleMeta.getItems().isEmpty())
-				return true;
-		}
-
-		for (Map.Entry<Enchantment, Integer> enchant : meta.getEnchants().entrySet()) {
+		for (Map.Entry<Enchantment, Integer> enchant : meta.getEnchants().entrySet()) { // For any enchantments on an item, check if they are above their max set limit
 			if (enchant.getKey().getMaxLevel() < enchant.getValue())
 				return true;
 		}
 
-		if (meta.hasAttributeModifiers()
-				|| meta.hasEquippable()
-				|| meta.hasFood()
-				|| meta.hasItemName()
-				|| meta.hasItemModel()
-				|| meta.hasJukeboxPlayable()
-				|| meta.hasMaxStackSize()
-				|| meta.hasRarity()
-				|| meta.hasTool()
-				|| meta.hasTooltipStyle()
-				|| meta.hasUseCooldown()
-				|| meta.hasUseRemainder()
-				|| meta.hasDamageResistant()
-				|| meta.isUnbreakable()
-
-		)
-			return true;
+		return !item.matchesWithoutData(defaultItem, nbtToIgnore, true);
+	}
 
 		return false;
 	}
